8976118a7c9491f54ee152d3fef18f5472f6bb1d
Ethan <175902687+kokorocks@users.noreply.github.com>
2024-10-19 18:39:29 +0000

    made it work, yay

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+from gradio_client import Client
 
+client = Client("yuntian-deng/o1mini")
+result = client.predict(
+		inputs="Hello!!",
+		top_p=1,
+		temperature=1,
+		chat_counter=0,
+		chatbot=[],
+		api_name="/predict"
+)
+print(result)
